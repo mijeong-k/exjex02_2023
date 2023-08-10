8eba32317716bdad65c38a444c518f6bc8cbe34d
MJ <MJ@DESKTOP-3KOT7BS>
2023-08-10 11:02:15 +0900

    jex02_jUnit테스트

--- jex02/src/main/java/org/zerock/config/RootConfig.java
+++ jex02/src/main/java/org/zerock/config/RootConfig.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.zaxxer.hikari.HikariConfig;
